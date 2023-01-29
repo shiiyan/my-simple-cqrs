c6f7e75bdcda059d61e8d112d6f79bf5035281f9
shiiyan <yan39123@gmail.com>
2023-01-29 16:33:44 +0900

    feat: implement product repository

--- src/infra/productRepository.ts
+++ src/infra/productRepository.ts
@@ -2,9 +2,13 @@ import Product from "../domain/product";
 import ProductRepositoryInterface from "../domain/productRepositoryInterface";
 
 export default class ProductRepository implements ProductRepositoryInterface {
+  private productMap: Map<number, Product> = new Map();
+
   findById(id: number): Product | null {
-    return null;
+    return this.productMap.get(id) ?? null;
   }
 
-  save() {}
+  save(product: Product): void {
+    this.productMap.set(product.getId(), product);
+  }
 }
